f4c7af2796fd6bca5249de4fcd7d6a95c889e50c
reddavis <reddavis@gmail.com>
2010-09-12 18:13:52 +0100

    remove the .git from the demo

--- example/generator.rb
+++ example/generator.rb
@@ -11,3 +11,8 @@ export_path = File.expand_path(File.dirname(__FILE__))
 
 html = File.read("#{example_dir}/documentation.html")
 ArpiGemBuilder::Generator.new(html).generate(example_dir)
+
+# Just remove the git stuff as it fucks stuff up
+FileUtils.cd("#{example_dir}/embedit") do |dir|
+  FileUtils.rm_r("#{dir}/.git")
+end
